df71d2f31fca1faccc7cc119db3d12ebb116a50c
Clara Genovese <clariigenovese@gmail.com>
2022-08-28 17:38:47 -0300

    Structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Nav from './components/Nav'
+import Hero from './pages/Hero'
+import Services from './pages/Services'
+import About from './pages/About'
+import Clients from './pages/Clients'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Hero />
+      <Services />
+      <About />
+      <Clients />
+      <Footer />
     </div>
   )
 }
---
+++ src/websiteContent.js
@@ -0,0 +1,23 @@
+export const nav = {
+    
+}
+
+export const hero = {
+
+}
+
+export const services = {
+    
+}
+
+export const about = {
+    
+}
+
+export const clients = {
+    
+}
+
+export const footer = {
+    
+}
